a3eb8812333b3a7c5ae70f580b4f8e999b7787b6
blindthief10 <kostasdiakogiannis23@gmail.com>
2020-02-05 16:51:03 +0100

    lineups start ready

--- src/components/DetailedMatch.vue
+++ src/components/DetailedMatch.vue
@@ -1,7 +1,36 @@
 <template>
     <div class="container mb-4">
         <p class="lead display-4">{{selectedMatchInfo.description}}</p>
-        <p class="lead" v-if="selectedMatchInfo.status == 'Fixture'">{{this.dateGameStarts}}</p>
+        <p class="lead" v-html="competitonDescription"></p>
+        <p class="lead" v-if="selectedMatchInfo.status === 'Fixture'" v-html="dateGameStarts"></p>
+        <div v-if="selectedMatchInfo.status === 'Played'" class="d-flex flex-column justify-content-between">
+          <h3 class="lead display-3 text-center my-5" v-text="finalScore"></h3>
+          <div class="d-flex flex-row justify-content-around">
+
+            <div class="card d-flex flex-column justify-content-between">
+              <div class="card-header bg-light">
+                <h4 class="title text-center" v-text="homeTeam"></h4>
+                <ul class="list-group my-4">
+                  <li class="list-group-item text-dark" v-for="homePlayer in selectedMatchInfo.lineups.home.start">
+                    {{homePlayer.shirt}}. {{homePlayer.name}}
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="card d-flex flex-column justify-content-between">
+              <div class="card-header bg-dark text-white">
+                <h4 class="title text-center" v-text="awayTeam"></h4>
+                <ul class="list-group my-4">
+                  <li class="list-group-item text-dark" v-for="awayPlayer in selectedMatchInfo.lineups.away.start">
+                    {{awayPlayer.shirt}}. {{awayPlayer.name}}
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+          </div>
+          <p class="lead" v-text="attendance"></p>
+        </div>
     </div>
 
 </template>
@@ -17,7 +46,23 @@
         return this.$store.getters.selectedMatch;
       },
       dateGameStarts() {
-        return `Game starts at: ${moment(this.selectedMatchInfo.date).format('dddd, MMMM Do YYYY, h:mm:ss a')}`
+        return `Game starts at: <strong>${moment(this.selectedMatchInfo.date)
+                            .format('dddd, MMMM Do YYYY, h:mm:ss a')}</strong>`
+      },
+      competitonDescription() {
+        return `Competition: <strong>${this.selectedMatchInfo.competition}</strong>`
+      },
+      finalScore() {
+        return `Final score: ${this.selectedMatchInfo.scores.total.home} - ${this.selectedMatchInfo.scores.total.away}`;
+      },
+      attendance() {
+        return `Attendance: ${this.selectedMatchInfo.attendance}`;
+      },
+      homeTeam() {
+        return `${this.selectedMatchInfo.hometeam}\'s starting lineup`;
+      },
+      awayTeam() {
+        return `${this.selectedMatchInfo.awayteam}\'s starting lineup`;
       }
     },
     created() {
